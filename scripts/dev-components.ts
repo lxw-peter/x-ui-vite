7c00f8243c34fe0b30e8f569d36022613356cae0
lxw-peter <1270542959@qq.com>
2023-01-03 22:36:28 +0800

    添加打包脚本

---
+++ scripts/dev-components.ts
@@ -0,0 +1,17 @@
+import { createServer } from 'vite';
+import { resolvePackagePath } from './util';
+import pluginVue from '@vitejs/plugin-vue';
+import pluginVueJsx from '@vitejs/plugin-vue-jsx';
+
+const server = await createServer({
+  // any valid user config options, plus `mode` and `configFile`
+  configFile: false,
+  root: resolvePackagePath('components'),
+  plugins: [pluginVue(), pluginVueJsx()],
+  server: {
+    port: 8080
+  }
+});
+await server.listen();
+
+server.printUrls();
